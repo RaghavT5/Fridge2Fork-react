fea73b34d8957937bedf68873ad57458ce8661ae
Raghav <49293356+RaghavT5@users.noreply.github.com>
2023-01-28 20:42:28 +0530

    Fixed most major bugs

--- src/components/CategoryCard.jsx
+++ src/components/CategoryCard.jsx
@@ -14,7 +14,7 @@ const CategoryCard = () => {
   };
 
   return (
-    <div className="grid grid-cols-3 gap-4 m-1 text-center font-poppins">
+    <div className="grid lg:grid-cols-3 grid-cols-2 gap-4 m-1 text-center font-poppins">
       {categoriesList.map((category) => (
         <div
           key={category.id}
@@ -25,7 +25,6 @@ const CategoryCard = () => {
               : setVisibleCategoryId(category.id)
           }
         >
-          {/* <span className="bg-red-500"> */}
           <div className="flex text-center pl-10 bg-[#f8f9fa] rounded-lg hover:scale-105 hover:bg-slate-200">
             <img
               src={category.img}
@@ -36,22 +35,24 @@ const CategoryCard = () => {
               {category.categoryName}
             </div>
           </div>
-          {/* </span> */}
         </div>
       ))}
       {visibleCategoryId && (
-        <div className="grid grid-cols-6 gap-4">
+        <div className="grid lg:grid-cols-6 md:grid-cols-3 gap-4">
           {categoriesList
             .filter((category) => category.id === visibleCategoryId)
             .map((category) => (
-              <div key={category.id} className="grid grid-cols-6 gap-x-28">
+              <div
+                key={category.id}
+                className="grid lg:grid-cols-6 md:grid-cols-3 gap-x-28 md:text-xl lg:text-xs"
+              >
                 {category.ingredients.map((ingredient, index) => (
                   <div
                     key={ingredient}
                     className="cursor-pointer "
                     onClick={() => handleIngredientClick(ingredient)}
                   >
-                    <span className=" hover:bg-[#ffc107] hover:font-semibold px-2 py-1 text-xs">
+                    <span className=" hover:bg-[#ffc107] hover:font-semibold px-2 py-1 lg:text-xs md:text-base">
                       {ingredient}
                     </span>
                   </div>
